bd5de3a3b5eff57751170b8a1da7e8377a3c7e14
chensisi <2532692867@qq.com>
2024-01-05 16:55:48 +0800

    vue

--- vue/demo01-vue01/src/App.vue
+++ vue/demo01-vue01/src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
 
+import {reactive} from "vue";
 
+let user = reactive({
+  name: "zhangsan",
+  age: 18
+})
+
+let hbs = reactive([])
 
 </script>
 
 <template>
   <div>
-
+    <input type="text" v-model="user.name"/><br>
+    姓名：{{ user.name }}<br>
+    爱好：<br>
+    唱：<input type="checkbox" name="hbs" v-model="hbs" value="sing">
+    跳：<input type="checkbox" name="hbs" v-model="hbs" value="dance">
+    rap：<input type="checkbox" name="hbs" v-model="hbs" value="rap">
+    <br>
+    显示爱好： {{ hbs }}
   </div>
 
 </template>
