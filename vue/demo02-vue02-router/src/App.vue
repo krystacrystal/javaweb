1bd289c8ce84351086353b3c494f77f915cb75e9
chensisi <2532692867@qq.com>
2024-01-11 10:36:55 +0800

    vue

--- vue/demo02-vue02-router/src/App.vue
+++ vue/demo02-vue02-router/src/App.vue
@@ -6,7 +6,11 @@
   <div>
     <h1>APP页面</h1>
     <hr/>
-    <router-view></router-view>
+<!--    路由的连接-->
+    <router-link to="/">home页</router-link><br>
+    <router-link to="/list">list页</router-link><br>
+    <router-link to="/add">add页</router-link><br>
+    <router-link to="/update">update页</router-link><br>
 
   </div>
 
--- vue/demo03-router/src/main.js
+++ vue/demo03-router/src/main.js
@@ -1,8 +1,6 @@
 import {createApp} from 'vue'
-
 import App from './App.vue'
-import router from "./routers/router.js";
+
 let app = createApp(App)
-//绑定路由对象
-app.use(router)
+app.use()
 app.mount('#app')
